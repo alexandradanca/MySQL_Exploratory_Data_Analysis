fb1c3529689fc8061722624c91641ebc1be91858
Alexandra Danca <92735546+alexandradanca@users.noreply.github.com>
2024-12-10 13:28:24 +0200

    Add files via upload

--- data_analysis.sql
+++ data_analysis.sql
@@ -1,32 +1,32 @@
--- What date range was this database created for?  2020-03-11, 2023-03-06
+-- What date range was this database created for?
 SELECT MIN(`date`), MAX(`date`)
 FROM layoffs_db2;
 
--- Which company made the most layoffs? Amazon layoffs 18150 employee, next is Google with 12000 layoffs
+-- Which company made the most layoffs?
 SELECT company, SUM(total_laid_off)
 FROM layoffs_db2
 GROUP BY company
 ORDER BY 2 DESC;
 
--- Which industries were most affected by employee layoffs? Consumer, Retail, Other, Transportation
--- But which suffered the least? Manufacturing, Fin-Tech, Aerospace
+-- Which industries were most affected by employee layoffs?
+-- But which suffered the least?
 SELECT industry, SUM(total_laid_off)
 FROM layoffs_db2
 GROUP BY industry
 ORDER BY 2 DESC;
 
--- Which conutry made the most layoffs? United Sates followed by India and Netherlands
+-- Which conutry made the most layoffs?
 SELECT country, SUM(total_laid_off)
 FROM layoffs_db2
 GROUP BY country
 ORDER BY 2 DESC;
 
--- Which company made layoffs in Romania? Foodpanda
+-- Which company made layoffs in Romania?
 SELECT *
 FROM layoffs_db2
 WHERE country = 'Romania';
 
--- How have the layoffs been over the years? More(2022 with 160661, 2023 with 125677, 2020 with 80998 and 2021 with 15823)Less
+-- How have the layoffs been over the years?
 SELECT YEAR(`date`) AS year, SUM(total_laid_off)
 FROM layoffs_db2
 GROUP BY year
@@ -48,4 +48,3 @@ WHERE `year` IS NOT NULL
 SELECT * 
 FROM Company_Year_Rank
 WHERE Ranking <= 5;
-
